b056b8cfa068afc6f9a5094c35cdfa7aa48d8072
Programming Hero <programming.hero1@gmail.com>
2025-03-21 17:23:44 -0500

    load data using use api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import Counter from './Counter';
 import Batsman from './Batsman';
 import Users from './Users';
+import Friends from './Friends';
 import './App.css'
+import { Suspense } from 'react';
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+
+const fetchFriends = async() =>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  return res.json();
+}
 
 function App() {
+  const friendsPromise = fetchFriends();
 
   function handleClick() {
     alert('I am clicked.')
@@ -13,7 +24,7 @@ function App() {
     alert('clicked 3')
   }
 
-  const handleAdd5 = (num) =>{
+  const handleAdd5 = (num) => {
     const newNum = num + 5;
     alert(newNum);
   }
@@ -22,14 +33,20 @@ function App() {
     <>
       <h3>Vite + React</h3>
 
-      <Users></Users>
+      {/* <Suspense fallback={<h3>Loading...</h3>}>
+        <Users fetchUsers ={fetchUsers}></Users>
+      </Suspense> */}
+
+      <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+        <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense>
 
       <Batsman></Batsman>
       <Counter></Counter>
 
       {/* <button onclick="handleClick()">Click Me</button> */}
       <button onClick={handleClick}>Click Me</button>
-      <button onClick={function handleClick2(){
+      <button onClick={function handleClick2() {
         alert('clicked 2')
       }}>Click Me2</button>
 
