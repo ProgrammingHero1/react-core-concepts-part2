7423340cd2c8ab690156817e8a98cb1d9c6bcf64
Programming Hero <programming.hero1@gmail.com>
2025-03-21 16:26:31 -0500

    added users component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Counter from './Counter';
 import Batsman from './Batsman';
+import Users from './Users';
 import './App.css'
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
   return (
     <>
       <h3>Vite + React</h3>
+
+      <Users></Users>
+
       <Batsman></Batsman>
       <Counter></Counter>
 
